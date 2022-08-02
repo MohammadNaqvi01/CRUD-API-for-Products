4c5a75612205e067a81fc407c95c99a96a408a7c
Ali <syedali2979@gmail.com>
2022-08-02 21:34:00 +0530

    Api Documentation Added

--- EcomStore/products/serializers.py
+++ EcomStore/products/serializers.py
@@ -28,9 +28,3 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id','category','product_name','image','price','description','stock','color_type','size_type']
         depth = 1
-    # def create(self, validated_data):
-    #     category = validated_data.pop('category')
-    #     products = Product.objects.create(**validated_data)
-    #     for product in category:
-    #         Category.objects.create(product=product, **category)
-    #     return products
--- EcomStore/products/views.py
+++ EcomStore/products/views.py
@@ -14,7 +14,7 @@ from rest_framework import status
 
 class ProductView(ModelViewSet):
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]   
+    # permission_classes = [IsAuthenticated]   
     
 
     def get_queryset(self):
@@ -23,20 +23,19 @@ class ProductView(ModelViewSet):
         return product
 
     def create(self, request, *args, **kwargs):
-        serializer = ProductSerializer(data=request.data)
- 
-        if serializer.is_valid():
-         
-            try:
-              
-                serializer.save()
-               
-            except Exception as e:
-                return Response({'error':str(e)})
-
-            return Response({'data':serializer.data,'status':status.HTTP_201_CREATED})
-        return Response({'error':serializer.errors, 'status':status.HTTP_400_BAD_REQUEST})
-
+        data = request.data
+    
+        try:
+          
+            new_product = Product.objects.create(category=Category.objects.get(category_name=data['category']),
+            product_name=data['product_name'],image=data['image'],price=data['price'],description=data['description'],
+            stock=data['stock'],color_type=ColorVariant.objects.get(color_name=data['color_type']),
+            size_type=SizeVariant.objects.get(size_name=data['size_type']))
+            new_product.save()
+            serializer = ProductSerializer(new_product)
+            return Response({'data':serializer.data, 'status':status.HTTP_201_CREATED})
+        except Exception as e:
+            return Response({'status':repr(e)})
 
     def update(self, request, *args, **kwargs):
         product = self.get_object()
@@ -50,13 +49,26 @@ class ProductView(ModelViewSet):
    
      
     def partial_update(self, request, *args, **kwargs):
+        data = request.data
         product = self.get_object()
-        serializer = ProductSerializer(product, data=request.data)      
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'data':serializer.data,'status':status.HTTP_201_CREATED})
-        return Response({'error':serializer.errors, 'status':status.HTTP_400_BAD_REQUEST})
+        try:
+          
+            product.category=Category.objects.get(category_name=data['category'])
+            product.color_type=ColorVariant.objects.get(color_name=data['color_type'])
+            product.size_type=SizeVariant.objects.get(size_name=data['size_type'])
+        except KeyError:
+           pass  
+
+        product.product_name=data.get('product_name',product.product_name)
+        product.image=data.get('image', product.image)
+        product.price=data.get('price',product.price)
+        product.description=data.get('description', product.description)
+        product.stock=data.get('stock', product.stock)
+          
+        product.save()
+        serializer = ProductSerializer(product)
+        return Response({'data':serializer.data, 'status':status.HTTP_201_CREATED})
+     
 
     def destroy(self, request, *args, **kwargs):
         product = self.get_object()
